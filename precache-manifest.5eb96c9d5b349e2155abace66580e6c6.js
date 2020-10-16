b27cbe80130b7b35a248dba75dc8412e16b41631
Sourav Mishra <mishrasourav81@gmail.com>
2020-10-16 18:09:03 +0530

    Updates

---
+++ precache-manifest.5eb96c9d5b349e2155abace66580e6c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d22014619b541fdac6698b0363632d88",
+    "url": "/Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "9cd1ad87fd94b7065045",
+    "url": "/Covid-19-Tracker/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "1b4d547424fba27cbd86",
+    "url": "/Covid-19-Tracker/static/css/main.2c805c82.chunk.css"
+  },
+  {
+    "revision": "9cd1ad87fd94b7065045",
+    "url": "/Covid-19-Tracker/static/js/2.7254861e.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/Covid-19-Tracker/static/js/2.7254861e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b4d547424fba27cbd86",
+    "url": "/Covid-19-Tracker/static/js/main.36d1e7dc.chunk.js"
+  },
+  {
+    "revision": "27e1b2f6f184041c1ded",
+    "url": "/Covid-19-Tracker/static/js/runtime-main.3c792d04.js"
+  }
+]);
